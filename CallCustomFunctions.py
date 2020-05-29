f4bba2e36049b431f115585b79ff8dfee3c5257c
Joseph C. Richardson <34896540+Cozmo1@users.noreply.github.com>
2020-05-29 03:08:20 -0400

    Add files via upload

---
+++ CallCustomFunctions.py
@@ -0,0 +1,13 @@
+print('Call Custom Functions from another file example.\n')
+
+from CustomFunctions import (
+    custom_function1,
+    custom_function2,
+    custom_function3
+    )
+
+custom_function1()
+custom_function2()
+custom_function3()
+
+
---
+++ CustomFunctions.py
@@ -0,0 +1,8 @@
+def custom_function1():
+    print('You called custom_function1')
+
+def custom_function2():
+    print('You called custom_function2')
+
+def custom_function3():
+    print('You called custom_function3')
